dcdeafcb66775a5528f063d047ad6135960ee31d
fernando Urbano <fernando_urbano@outlook.com>
2025-08-30 00:37:40 -0300

    Proyecto finalizado

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -3,7 +3,9 @@ import LogoHithub from "../img/Github.svg";
 
 export const QrCode = () => {
   return (
-    <>
+    <> 
+
+
     <div className="mt-10 bg-amber-50 p-5 rounded-2xl h-auto w-[280px] shadow-sm">
          <div className="flex flex-col text-center m-auto p-5  bg-[#3784dd] rounded-1xl rounded-sm">
         <QRCode
@@ -14,10 +16,15 @@ export const QrCode = () => {
         />
       </div>
 
+
+
       <div className="flex justify-around py-5">
         <img src={LogoHithub} alt="" className="size-7" />
         <h1 className="flex">Mi Repositorio de Git-Hub</h1>
       </div>
+
+
+      
       <p className="text-center text-neutral-700">
         Escanea el codigo Qr y podras ingresar a mi pagina de HitHub actualizada , donde podras encontrar todos mis repositorios
 
