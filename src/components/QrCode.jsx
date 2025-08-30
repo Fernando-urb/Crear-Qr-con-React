a05d77d20e85b72d9b9dc6cedc1e8938a56aa38e
fernando Urbano <fernando_urbano@outlook.com>
2025-08-30 09:54:18 -0300

    solucion de poblemas

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -3,28 +3,28 @@ import LogoHithub from "../img/Github.svg";
 
 export const QrCode = () => {
   return (
-    <> 
+    < > 
+    
+       <div className=" bg-amber-50 p-5 rounded-2xl h-auto  w-[250px] shadow-sm  contenedor-qr flex flex-col gap-4 ">
 
-
-    <div className="mt-10 bg-amber-50 p-5 rounded-2xl h-auto w-[280px] shadow-sm">
          <div className="flex flex-col text-center m-auto p-5  bg-[#3784dd] rounded-1xl rounded-sm">
-        <QRCode
+        <QRCode className="qr"
           value="https://github.com/Fernando-urb"
           bgColor="#3784dd"
           fgColor="white"
-          size="200"
+          size="150"
         />
       </div>
 
 
 
-      <div className="flex justify-around py-5">
-        <img src={LogoHithub} alt="" className="size-7" />
-        <h1 className="flex">Mi Repositorio de Git-Hub</h1>
+      <div className="flex justify-center gap-3 ">
+        <img src={LogoHithub} alt="" className=" size-5" />
+        <h1 className="text-[0.8rem] ">Mi Repositorio de Git-Hub</h1>
       </div>
 
 
-      
+
       <p className="text-center text-neutral-700">
         Escanea el codigo Qr y podras ingresar a mi pagina de HitHub actualizada , donde podras encontrar todos mis repositorios
 
@@ -32,6 +32,11 @@ export const QrCode = () => {
 
     </div>
      
+
+    
+
+
+   
 
       
     </>
